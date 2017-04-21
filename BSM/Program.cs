84d3573fd27a9ac96fddad88745efa364ab97614
John Du Hart <john@johnduhart.me>
2017-04-21 11:34:03 -0400

    Adding global exception handler

--- BSM/Program.cs
+++ BSM/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Serilog;
 
@@ -16,12 +13,15 @@ namespace Borealis
         static void Main()
         {
             ConfigureLogging();
+            ConfigureExceptionHandling();
 
             Log.Information("Starting Borealis Server Manager {version} at {time}", Application.ProductVersion, DateTime.Now);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BorealisServerManager());
+
+            Log.Information("Borealis Server Manager stopping.");
         }
 
         private static void ConfigureLogging()
@@ -31,6 +31,28 @@ namespace Borealis
                 .WriteTo.Trace()
                 .WriteTo.File("Borealis-log.txt")
                 .CreateLogger();
+        }
+
+        private static void ConfigureExceptionHandling()
+        {
+            Application.ThreadException += (sender, args) =>
+            {
+                Log.Fatal(args.Exception, "Unhandled exception occurred in the main UI thread");
+            };
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                var exception = args.ExceptionObject as Exception;
+                if (exception == null)
+                {
+                    Log.Fatal("Unknown exception type was passed to the app domain exception handler.");
+                    return;
+                }
+
+                Log.Fatal(exception, "Unhandled exception occurred");
+            };
         }
     }
 }
