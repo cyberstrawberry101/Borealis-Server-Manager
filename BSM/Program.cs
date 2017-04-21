fc260a3a72fa0c34da90725207121da709f73429
John Du Hart <john@johnduhart.me>
2017-04-21 11:14:47 -0400

    Adding logging using Serilog

--- BSM/Program.cs
+++ BSM/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Serilog;
 
 namespace Borealis
 {
@@ -14,9 +15,22 @@ namespace Borealis
         [STAThread]
         static void Main()
         {
+            ConfigureLogging();
+
+            Log.Information("Starting Borealis Server Manager {version} at {time}", Application.ProductVersion, DateTime.Now);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BorealisServerManager());
+        }
+
+        private static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose() // For now
+                .WriteTo.Trace()
+                .WriteTo.File("Borealis-log.txt")
+                .CreateLogger();
         }
     }
 }
